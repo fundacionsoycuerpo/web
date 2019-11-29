c09e33ca5b0d42aa88b609ca6ba76d74fb5f4b78
Ignacio Le Fluk <flakolefluk@gmail.com>
2019-11-29 11:41:15 -0300

    Dev (#5)
    * bump dependencies
    
    * somos cerro initial commit
    
    * change date

--- src/routes/eventos/_next.js
+++ src/routes/eventos/_next.js
@@ -2,8 +2,8 @@ const events = [
     {
       title: "Somos Cerro",
       description: "1ª Jornada de auto-cuidado en espacios naturales.",
-      date: "Sábado 9 de noviembre",
-      time: "10:00",
+      date: "Pronto...",
+      time: "",
       location: "Vivero Cumbre - Parque Metropolitano",
       img: "img/somos_cerro - pronto.png",
       imgAlt: "Afiche Somos Cerro - Pronto",
