b41df4210e41c36812b1a6289cd4ef0d993c3bfd
Ignacio Falk <flakolefluk@gmail.com>
2020-01-15 03:09:03 -0300

    box-shadow

--- src/components/Member.svelte
+++ src/components/Member.svelte
@@ -1,5 +1,5 @@
 <script>
-  import { createEventDispatcher } from "svelte";
+  import { createEventDispatcher } from 'svelte';
 
   export let member;
   export let expanded = false;
@@ -8,7 +8,7 @@
   const dispatch = createEventDispatcher();
 
   function expand(event) {
-    dispatch("expand", expanded ? null : member.email);
+    dispatch('expand', expanded ? null : member.email);
   }
 </script>
 
@@ -16,7 +16,8 @@
   .card {
     display: flex;
     flex-direction: column;
-    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+    box-shadow: 0 0 0 1px rgba(255, 255, 255, 0.1),
+      0 2px 4px 0 rgba(14, 30, 37, 0.12);
     transition: 0.3s;
     padding: 1rem;
     border-radius: 5px;
