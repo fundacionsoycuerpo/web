b31ba793c5906b5efccade8ccc3e5566140b75b5
Ignacio Le Fluk <flakolefluk@gmail.com>
2020-07-22 18:49:09 -0400

    Resize logo - Bump Deps (#42)
    * bump dependencies
    
    * resize logo, use srcset

--- src/routes/galeria/index.json.js
+++ src/routes/galeria/index.json.js
@@ -1,9 +1,9 @@
-import galleries from '../data/_galerias';
+import galleries from '../_data/_galerias';
 
 export function get(req, res) {
   const content = JSON.stringify(galleries);
   res.writeHead(200, {
-    'Content-Type': 'application/json'
+    'Content-Type': 'application/json',
   });
   res.end(content);
 }
