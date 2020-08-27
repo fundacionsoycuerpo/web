8f6a009d9b77ade6a32ca7029cbc4391213a30d6
Ignacio Le Fluk <flakolefluk@gmail.com>
2020-08-27 01:22:38 -0400

    fix event description (#44)

--- src/routes/_data/_eventos.js
+++ src/routes/_data/_eventos.js
@@ -1,7 +1,7 @@
 const events = [
   {
     title: 'El Cuerpo de la Memoria 2020',
-    description: `El despertar de Chile, el estallido social, la revuelta.\n\nEncuentro de Danza, Performance y reflexión.\n\nUna alianza entre Fundación Soy Cuerpo y Centro Cultural de España\n\n_"Es porque el pasado es inconcluso que el trabajo residual de la memoria se mueve de escena en escena, a la búsqueda retrospectiva de aquellas intermitencias que aún contienen energías latentes"_ (Nelly Richard. Crítica de la memoria)`,
+    description: `El despertar de Chile.\n\nEncuentro de Danza, Performance y reflexión.\n\nUna alianza entre Fundación Soy Cuerpo y Centro Cultural de España\n\n_"Es porque el pasado es inconcluso que el trabajo residual de la memoria se mueve de escena en escena, a la búsqueda retrospectiva de aquellas intermitencias que aún contienen energías latentes"_ (Nelly Richard. Crítica de la memoria)`,
     date: '20 de octubre del 2020',
     time: '',
     location: '',
