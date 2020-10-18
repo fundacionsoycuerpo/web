d812f9310fac3a15d942c96755f0368dda9370db
Ignacio Falk <flakolefluk@gmail.com>
2020-10-18 16:51:20 -0300

    update event image

--- src/routes/_data/_eventos.js
+++ src/routes/_data/_eventos.js
@@ -1,11 +1,11 @@
 const events = [
   {
     title: 'El Cuerpo de la Memoria 2020',
-    description: `El despertar de Chile.\n\nEncuentro de Danza, Performance y reflexión.\n\nUna alianza entre Fundación Soy Cuerpo y Centro Cultural de España\n\n_"Es porque el pasado es inconcluso que el trabajo residual de la memoria se mueve de escena en escena, a la búsqueda retrospectiva de aquellas intermitencias que aún contienen energías latentes"_ (Nelly Richard. Crítica de la memoria)`,
+    description: `El despertar de Chile.\n\nEncuentro de Danza, Performance y reflexión.\n\nInvita Fundación Soy Cuerpo con la colaboración de Centro Cultural de España, FAE Festival de Arte Erótico y La Perrera Arte.\n\n_"Es porque el pasado es inconcluso que el trabajo residual de la memoria se mueve de escena en escena, a la búsqueda retrospectiva de aquellas intermitencias que aún contienen energías latentes"_ (Nelly Richard. Crítica de la memoria)\n\nTransmisión por el <a href="https://fundacionsoycuerpo.cl/youtube">Canal de Youtube Fundación Soy Cuerpo</a>`,
     date: '20 de octubre del 2020',
-    time: '',
+    time: '19:00',
     location: '',
-    img: 'img/el_cuerpo_de_la_memoria_2020-pronto-2.jpg',
+    img: 'img/el_cuerpo_de_la_memoria_2020.jpg',
     imgAlt: 'Afiche El Cuerpo de la Memoria 2020',
   },
 ];
