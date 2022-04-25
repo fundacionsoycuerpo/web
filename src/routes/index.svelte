bc9333312c3ffeaf2b09c1cb8a07d8db0ab99d7c
Ignacio Falk <flakolefluk@gmail.com>
2022-04-25 19:17:39 -0400

    restore

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -168,17 +168,17 @@
 		position: relative;
 		padding: 40px 0px 80px;
 		background-color: black;
-		// margin-left: -2rem;
-		// padding-left: 2rem;
-		// padding-right: 2rem;
+		margin-left: -2rem;
+		padding-left: 2rem;
+		padding-right: 2rem;
 		width: 100%;
-		// max-width: 1400px;
+		max-width: 1400px;
 		color: white;
-		// @media (min-width: 490px) {
-		// 	margin-left: -5rem;
-		// 	padding-left: 5rem;
-		// 	padding-right: 5rem;
-		// }
+		@media (min-width: 490px) {
+			margin-left: -5rem;
+			padding-left: 5rem;
+			padding-right: 5rem;
+		}
 	}
 
 	// .video-section > h2,
